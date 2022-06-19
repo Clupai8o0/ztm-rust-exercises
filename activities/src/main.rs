a709061e58fdd3315718369f2c6e13c4116849c9
Clupai8o0 <clupaio4@gmail.com>
2022-06-19 19:53:00 +0300

    exercises

---
+++ activities/src/main.rs
@@ -0,0 +1,19 @@
+// Topic: Functions
+// Program requirements:
+// * Displays your first and last name
+
+// * Use a function to display your first name
+fn first_name() {
+  println!("Samridh");
+}
+
+// * Use a function to display your last name
+fn last_name() {
+  println!("Limbu")
+}
+
+// * Use the println macro to display messages to the terminal
+fn main() {
+  first_name();
+  last_name();
+}
